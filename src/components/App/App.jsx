4b92f7e18970f354162b3ded56a1a099d7802174
Ivan Moshak <ivanmoshak11@gmail.com>
2024-02-18 10:09:28 +0200

    updated

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,16 @@
+import ContactForm from '../ContactForm/ContactForm.jsx';
+import SearchBox from '../SearchBox/SearchBox.jsx';
+import ContactList from '../ContactList/ContactList.jsx';
+
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   )
 }
 
